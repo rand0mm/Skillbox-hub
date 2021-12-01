6ee334c622ce40088466a98795de2644012cae55
dmitriy <kozhin.rndm@gmail.com>
2021-12-01 11:35:56 +0300

    change web-adv rep

--- javaScript/kursach/js/localStorage.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const restoredSession = JSON.parse(localStorage.getItem('savedDataArr'));
-  if (restoredSession) {
-    savedDataArr = restoredSession;
-    unsortedArr = savedDataArr.slice();
-  } else { savedDataArr = dataArr; }
-  // makeTable(constructorArr(sorted, savedDataArr));
-});
