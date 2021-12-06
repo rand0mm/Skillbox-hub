a55fc595823fcd191ed5a264b5b84fa044d09b3f
dmitriy <kozhin.rndm@gmail.com>
2021-12-06 16:54:20 +0300

    add: js-intens

---
+++ javaScript/js-intens/01_types.js
@@ -0,0 +1,46 @@
+// null, undefined, boolean, number, string, object, symbol
+
+// console.log(typeof 0)
+// console.log(typeof true)
+// console.log(typeof 'Javascript')
+// console.log(typeof undefined)
+// console.log(typeof Math)
+// console.log(typeof Symbol('JS'))
+// console.log(typeof null)
+// console.log(typeof function() {})
+// console.log(typeof null)
+// console.log(typeof NaN)
+
+// Приведение типов
+// let language = 'JavsScript'
+
+// if (language) {
+//     console.log('The best language is:', language)
+// }
+
+// false values
+// '', 0, null, undefined, NaN, false
+
+// Строки и числа...
+
+// == vs === строгое не и строгое
+
+// =====
+console.log(false == '')
+console.log(false == [])
+console.log(false == {})
+console.log('' == 0)
+console.log('' == [])
+console.log('' == {})
+console.log(0 == [])
+console.log(0 == {})
+console.log(0 == null)
+
+
+
+
+
+
+
+
+
