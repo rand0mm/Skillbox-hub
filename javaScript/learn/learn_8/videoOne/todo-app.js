017464bd6eeb92c08509c749902655d868592d26
John Doe <daletters01@gmail.com>
2021-12-18 13:19:22 +0300

    change: js 8 lessons and add comment lauout pro (not full)

---
+++ javaScript/learn/learn_8/videoOne/todo-app.js
@@ -0,0 +1,56 @@
+(function() {
+  //создаем и возвращаем заголовок приложения
+  function createAppTitle(title) {
+    let appTitle = document.createElement('h2');
+    appTitle.innerHTML = title;
+    return appTitle;
+  }
+
+ //создаем и возвращаем форму для создания дела
+  function createTodoItemForm() {
+    let form = document.createElement('form');
+    let input = document.createElement('input');
+    let buttonWrapper = document.createElement('div');
+    let button = document.createElement('button');
+
+
+    form.classList.add('input-group', 'mb-3');
+    input.classList.add('form-control');
+    input.placeholder = 'Введите название нового дела';
+    buttonWrapper.classList.add('input-group-append');
+    button.classList.add('btn', 'btn-primary');
+    button.textContent = 'Добавить дело';
+
+    buttonWrapper.append(button);
+    form.append(input);
+    form.append(buttonWrapper);
+
+    return {
+      form,
+      input,
+      button,
+    };
+  }
+
+  //создаем и возвращаем заголовок приложения
+  function createTodoList() {
+    let list = document.createElement('ul');
+    list.classList.add('list-group');
+    return list;
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let container = document.getElementById('todo-app');
+
+    let todoAppTitle = createAppTitle('Список дел');
+    let todoItemForm = createTodoItemForm();
+    let todoList = createTodoList();
+
+    container.append(todoAppTitle);
+    container.append(todoItemForm.form);
+    container.append(todoList);
+  });
+})();
+//скобки в конце вызывают эту функцию, они важны!
+
+
