f40f357cf62e3da1288bcf29bd2b583a82b92ccc
dmitriy <kozhin.rndm@gmail.com>
2022-01-18 09:16:22 +0300

    add webpack

---
+++ learn-map/demka/webpack-demo/src/js/modal.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-underscore-dangle */
+function modalFunc() {
+  const openButton = document.querySelector('.js-open-modal');
+  const modal = document.querySelector(openButton.dataset.target); // #modal
+
+  openButton.addEventListener('click', () => {
+    modal.style.display = 'block';
+  });
+
+  modal.querySelector('.modal-dialog').addEventListener('click', (event) => {
+    event._isClickWithModal = true;
+  });
+
+  modal.addEventListener('click', (event) => {
+    if (event._isClickWithModal) return;
+    modal.style.display = 'none';
+  });
+}
+
+export default modalFunc;
