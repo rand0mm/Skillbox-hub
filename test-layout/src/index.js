02bed8f2d460d9b60aa2522f62531bbd82ba2f2a
dmitrii_kozhin <Kozhin.rndm@gmail.com>
2022-01-20 02:56:58 +0300

    add top-content on test

--- test-layout/src/index.js
+++ test-layout/src/index.js
@@ -1,1 +1,29 @@
 import './main.scss';
+import Swiper, { EffectFade, Thumbs } from 'swiper';
+import Choices from 'choices.js';
+
+Swiper.use([EffectFade, Thumbs]);
+
+const swiper = new Swiper('.left-box__slider', {
+  fadeEffect: {
+    crossFade: true,
+  },
+  touchRatio: 0,
+  spaceBetween: 0,
+  thumbs: {
+    swiper: {
+      direction: 'vertical',
+      el: '.left-box__mini-image-slider',
+      slidesPerView: 5,
+      spaceBetween: 7,
+    },
+  },
+});
+const choicesElement = document.querySelector('.choices');
+const choices = new Choices(choicesElement, {
+  silent: true,
+  searchEnabled: false,
+  position: 'bottom',
+  shouldSort: false,
+  itemSelectText: '',
+});
