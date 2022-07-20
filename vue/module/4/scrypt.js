49e903bc589f793633e791396df14b82f063bdfe
John Doe <daletters01@gmail.com>
2022-07-20 23:05:46 +0300

    add not full

--- vue/module/4/scrypt.js
+++ vue/module/4/scrypt.js
@@ -1,12 +1,13 @@
-Vue.component('like-dislike-buttons', {
-  props: ['counterlike','counterdislike'],
-  template: `<span><button type="button" @click="incrementLike">&#9829; {{counterlike}}</button><button type="button" @click="incrementDisLike">&#128078; {{counterdislike}}</button></span>`,
+Vue.component('like-button', {
+  model: {
+    prop: 'counter',
+    event: 'counter-change'
+  },
+  props: ['counter'],
+  template: `<button type="button" @click="increment">&#9829; {{counter}}</button>`,
   methods: {
     increment(){
-      this.$emit('update:counterlike', this.counterlike + 1);
-    },
-    incrementDisLike(){
-      this.$emit('update:counterdislike', this.counterdislike + 1);
+      this.$emit('counter-change', this.counter + 1);
     },
   }
 });
@@ -19,7 +20,7 @@ Vue.component('tasks-list', {
     <div class="item" :class="{done: task.done}" v-for="task in tasks" :key="task.text">
       <input type="checkbox" v-model="task.done">
       {{ task.text }}
-      <like-dislike-buttons :counterlike="task.likes" :counterdislike="task.dislikes"></like-dislike-buttons>
+      <like-button :counter.sync="task.likes"></like-button>
     </div>
   </div>`
 });
@@ -32,12 +33,10 @@ var app = new Vue({
     message: 'Привет, Vue!',
     likesHeader: 2,
     likesForm: 3,
-    likesDisHeader: 1,
-    likesDisForm: 0,
     tasks: [
-      {text: 'Развернуть окружение в Codepen', done: true, likes: 2, dislikes: 3},
-      {text: 'Пройти курс по Vue', done: false, likes: 1, dislikes: 3},
-      {text: 'Сделать интернет-магазин на Vue', done: false, likes: 4, dislikes: 2},
+      {text: 'Развернуть окружение в Codepen', done: true, likes: 2},
+      {text: 'Пройти курс по Vue', done: false, likes: 1},
+      {text: 'Сделать интернет-магазин на Vue', done: false, likes: 4},
     ],
     tasksTwo: [
       'Использовать знания в реальных проектах',
@@ -63,9 +62,6 @@ var app = new Vue({
     },
     countLikes() {
       return this.likesHeader + this.likesForm + this.tasks.reduce((value, task) => value + task.likes, 0);
-    },
-    countDisLikes() {
-      return this.likesDisHeader + this.likesDisForm + this.tasks.reduce((value, task) => value + task.likesDis, 0);
-    },
+    }
   }
 });
